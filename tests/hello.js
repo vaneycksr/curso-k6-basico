0d02ac4d8e646ac7d04c01995652935089294bb1
vaneycksr <vaneyck23@gmail.com>
2024-08-31 14:37:16 -0300

    add validation

--- tests/hello.js
+++ tests/hello.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 /* export const options = {
   vus: 10,
@@ -7,6 +7,11 @@ import { sleep } from 'k6';
 }; */
 
 export default function () {
-  http.get('http://localhost:3333');
+  const res = http.get('http://localhost:3333');
+
+  check(res, {
+    'status should be 200': (r) => r.status === 200
+  })
+
   sleep(1);
 }
